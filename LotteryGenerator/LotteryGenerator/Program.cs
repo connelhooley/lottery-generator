616552d569e19403b6017d0fbe0345122d81461b
Connel Hooley <connelhooley@gmail.com>
2017-11-24 14:25:17 +0000

    Inital commit

---
+++ LotteryGenerator/LotteryGenerator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace LotteryGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int countOfNumbersToGenerate = 5;
+            const int maxNumberToGenerate = 31;
+            Random random = new Random();
+            int[] numbers = Enumerable
+                .Repeat(0, countOfNumbersToGenerate)
+                .Select(i => random.Next(1, maxNumberToGenerate + 1))
+                .ToArray();
+            Console.WriteLine("Generated numbers:");
+            Console.WriteLine(string.Join(" ", numbers));
+            Console.WriteLine();
+            Console.WriteLine("Press enter to close");
+            Console.ReadLine();
+        }
+    }
+}
